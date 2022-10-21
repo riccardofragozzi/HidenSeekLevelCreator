4c474c6fd6fc3fdba1c0ee38403c0b9a27bcada5
riccardofragozzi <richi.fra@gmail.com>
2022-10-21 09:49:39 +0200

    first commit

---
+++ HidenSeekLevelCreator/MainWindow.xaml.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HidenSeekLevelCreator
+{
+
+    public partial class MainWindow : Window
+    {
+        private static Color INACTIVE_COLOR = Color.FromRgb(25, 25, 25);
+        private static Color BORDER_COLOR = Color.FromRgb(100, 100, 100);
+        private static Color ACTIVE_COLOR = Color.FromRgb(204, 153, 0);
+
+        private Dictionary<Point, Border> cells = new Dictionary<Point, Border>();
+        private OutputWindow outputWindow = new OutputWindow() { Visibility = Visibility.Collapsed };
+        private Point currPoint = new Point(0, 0);
+        public MainWindow()
+        {
+            InitializeComponent();
+            for(int y=0; y<1080/20; y++)
+            {
+                for(int x=0; x<1920/20; x++)
+                {
+                    Border cell = new Border()
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Background = new SolidColorBrush(INACTIVE_COLOR),
+                        BorderBrush = new SolidColorBrush(BORDER_COLOR),
+                        BorderThickness = new Thickness(0.2),
+                        CornerRadius = new CornerRadius(3),
+                        Margin = new Thickness(x*17, y*17, 0, 0),
+                        Width = 17,
+                        Height = 17
+                    };
+
+                    cell.MouseMove += (s, e) =>
+                    {
+                        if (draw_mode)
+                        {
+                            if (e.LeftButton == MouseButtonState.Pressed && cell.Tag == null)
+                            {
+                                handleColorChange(cell);
+                                cell.Tag = "handled";
+                            }
+                        }
+
+                        if (!draw_mode)
+                        {
+                            double xOffset = e.GetPosition(cell).X > cell.ActualWidth / 2 ? cell.ActualWidth : 0;
+                            double yOffset = e.GetPosition(cell).Y > cell.ActualHeight / 2 ? cell.ActualHeight : 0;
+                            cursorBorder.Margin = new Thickness(cell.Margin.Left + xOffset - 5, cell.Margin.Top + yOffset - 5, 0, 0);
+                            currPoint = new Point(Math.Round((cell.Margin.Left + xOffset) / 17.0, 0), Math.Round((cell.Margin.Top + yOffset) / 17.0));
+                            coordinateLbl.Content = currPoint.X + "," + currPoint.Y;
+                        }
+                    };
+
+                    cell.MouseLeave += (s, e) =>
+                    {
+                        if (draw_mode)
+                        {
+                            cell.Tag = null;
+                        }
+                    };
+
+                    cell.MouseDown += (s, e) =>
+                    {
+                        if (draw_mode)
+                        {
+                            handleColorChange((Border)s);
+                            cell.Tag = "handled";
+                        }
+                        if (!draw_mode)
+                        {
+                            outputWindow.appendPoint(currPoint);
+                        }
+                    };
+
+                    mainGrid.Children.Add(cell);
+                    cells[new Point(x, y)] = cell;
+                }
+            }
+
+            LocationChanged += (s, e) =>
+            {
+                outputWindow.Top = Top;
+                outputWindow.Left = Left + Width;
+            };
+
+            SizeChanged += (s, e) =>
+            {
+                outputWindow.Top = Top;
+                outputWindow.Height = Height;
+                outputWindow.Left = Left + Width;
+            };
+
+            Closing += (s, e) =>
+            {
+                outputWindow.Close();
+            };
+
+            cursorBorder.MouseDown += (s, e) =>
+            {
+                if (!draw_mode)
+                {
+                    outputWindow.appendPoint(currPoint);
+                }
+            };
+
+            funcBtn_Click(null, null);
+            funcBtn_Click(null, null);
+        }
+
+        private void handleColorChange(Border cell)
+        {
+            if (isActive(cell))
+            {
+                setActive(cell, false);
+            }
+            else
+            {
+                setActive(cell, true);
+            }
+        }
+
+        private bool isActive(Border cell)
+        {
+            return ((SolidColorBrush)cell.Background).Color.Equals(ACTIVE_COLOR);
+        }
+
+        private void setActive(Border cell, bool active)
+        {
+            cell.Background = new SolidColorBrush(active ? ACTIVE_COLOR : INACTIVE_COLOR);
+        }
+
+        private void loadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = string.Empty;
+            OpenFileDialog open = new OpenFileDialog() { Filter = "Configuration files|*.hsc" };
+            if (open.ShowDialog().Value)
+            {
+                filename = open.FileName;
+            }
+
+            if (filename == string.Empty)
+            {
+                return;
+            }
+
+            int currKey = 0;
+            foreach (byte ssbyte in System.IO.File.ReadAllBytes(filename))
+            {
+                setActive(cells[cells.Keys.ElementAt(currKey++)], ssbyte == 0xF);
+            }
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = string.Empty;
+            SaveFileDialog save = new SaveFileDialog() { Filter = "Configuration files|*.hsc" };
+            if (save.ShowDialog().Value)
+            {
+                filename = save.FileName;
+            }
+
+            if(filename == string.Empty)
+            {
+                return;
+            }
+
+            List<byte> bytes = new List<byte>();
+            foreach(Point cellPos in cells.Keys)
+            {
+                byte ssbyte = (byte)(isActive(cells[cellPos]) ? 0xF : 0xA);
+                bytes.Add(ssbyte);
+            }
+            System.IO.File.WriteAllBytes(filename, bytes.ToArray());
+        }
+        private bool draw_mode=true;
+        private void funcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            draw_mode = !draw_mode;
+            funcBtn.Content = draw_mode ? "Disegna" : "Seleziona";
+            cursorBorder.Visibility = draw_mode ? Visibility.Collapsed : Visibility.Visible;
+            coordinateLbl.Visibility = draw_mode ? Visibility.Collapsed : Visibility.Visible;
+            outputWindow.Visibility = draw_mode ? Visibility.Collapsed : Visibility.Visible;
+            outputWindow.Hide();
+            outputWindow.Show();
+        }
+    }
+}
---
+++ HidenSeekLevelCreator/OutputWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HidenSeekLevelCreator
+{
+    /// <summary>
+    /// Logica di interazione per OutputWindow.xaml
+    /// </summary>
+    public partial class OutputWindow : Window
+    {
+        private Point currOffset = new Point(-1, -1);
+        private Point firstVertex = new Point(-1, -1);
+        public OutputWindow()
+        {
+            InitializeComponent();
+            setInstruction();
+        }
+
+        private void setInstruction()
+        {
+            if(currOffset.X < 0)
+            {
+                instructionLbl.Content = "Scegli il punto offset in alto a sinistra del muro";
+            }
+            else
+            {
+                instructionLbl.Content = "Scegli tutti i vertici in sequenza";
+            }
+        }
+
+        public void appendPoint(Point point)
+        {
+            if(firstVertex.X>=0 && point.Equals(firstVertex))
+            {
+                MessageBox.Show("Poligono terminato.");
+                newPolygonBtn_Click(null, null);
+                return;
+            }
+            if (currOffset.X < 0) 
+            {
+                currOffset = point;
+                txtOut.Text += string.Format("\t<wall position=\"{0}\">", point.X * 17 + "," + point.Y * 17) + Environment.NewLine;
+                return;
+            }
+            if (firstVertex.X < 0)
+            {
+                firstVertex = point;
+            }
+
+            txtOut.Text += string.Format("\t\t<vertex position=\"{0}\"/>", (point.X - currOffset.X) * 17 + "," + (point.Y - currOffset.Y) * 17) + Environment.NewLine;
+            setInstruction();
+        }
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch(MessageBox.Show("Sicuro di voler eliminare tutto?", "", MessageBoxButton.YesNoCancel))
+            {
+                case MessageBoxResult.Yes:
+                    txtOut.Text = "";
+                    currOffset = new Point(-1, -1);
+                    firstVertex = new Point(-1, -1);
+                    setInstruction();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void newPolygonBtn_Click(object sender, RoutedEventArgs e)
+        {
+            currOffset = new Point(-1, -1);
+            firstVertex = new Point(-1, -1);
+            setInstruction();
+            txtOut.Text += "\t</wall>" + Environment.NewLine + Environment.NewLine;
+        }
+
+        private void copyOutputBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtOut.Text);
+        }
+    }
+}
